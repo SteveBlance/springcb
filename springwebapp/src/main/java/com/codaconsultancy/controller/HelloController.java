6fdbb7f27e9e6fb167ed7664ef02a60ab884cbf8
SteveBlance <stephen.blance@gmail.com>
2017-01-30 22:52:37 +0000

    Using a bean via @Autowired ApplicationContext

--- springwebapp/src/main/java/com/codaconsultancy/controller/HelloController.java
+++ springwebapp/src/main/java/com/codaconsultancy/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.codaconsultancy.controller;
 
 import com.codaconsultancy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,9 +13,19 @@ public class HelloController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @RequestMapping("hi")
     @ResponseBody
     public String hi() {
         return "nb of users: " + userService.findNumberOfUsers();
+    }
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello() {
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        return "nm of users: " + userService.findNumberOfUsers();
     }
 }
