b8ef8ffda3eebef04cc6cdb6675af6b3d40e6583
SteveBlance <stephen.blance@gmail.com>
2017-02-03 12:02:44 +0000

    Added titles to jsps from properties files

--- springwebapp/src/main/java/com/codaconsultancy/config/AppConfig.java
+++ springwebapp/src/main/java/com/codaconsultancy/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.codaconsultancy.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
@@ -21,6 +23,14 @@ public class AppConfig {
         final String[] definitions = {"/WEB-INF/tiles.xml"};
         tilesConfigurer.setDefinitions(definitions);
         return tilesConfigurer;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
     }
 
     // declare Tiles as a view resolver
--- springwebapp/src/main/webapp/WEB-INF/templates/template2.jsp
+++ springwebapp/src/main/webapp/WEB-INF/templates/template2.jsp
@@ -1,11 +1,19 @@
 <!DOCTYPE HTML>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<c:set var="titleKey">
+    <tiles:getAsString name="title"/>
+</c:set>
+
 <html>
 <head>
     <meta charset="utf-8">
+    <title><spring:message code="${titleKey}"/></title>
 </head>
 <body>
-<h1>Using Template 2</h1>
+<h1><spring:message code="${titleKey}"/></h1>
 <tiles:insertAttribute name="body"/>
 </body>
 </html>
