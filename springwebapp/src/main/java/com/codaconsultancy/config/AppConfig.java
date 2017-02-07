c7931ce33481fc4aff6eb2b8957e1a8baca501d6
SteveBlance <stephen.blance@gmail.com>
2017-02-07 21:02:52 +0000

    Added PerformanceInterceptor

--- springwebapp/src/main/java/com/codaconsultancy/config/AppConfig.java
+++ springwebapp/src/main/java/com/codaconsultancy/config/AppConfig.java
@@ -1,12 +1,16 @@
 package com.codaconsultancy.config;
 
+import com.codaconsultancy.controller.PerformanceInterceptor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
@@ -14,7 +18,7 @@ import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.codaconsultancy.controller",
         "com.codaconsultancy.service"})
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
 
     // declare Tiles configuration file
     @Bean
@@ -37,6 +41,18 @@ public class AppConfig {
     @Bean
     public ViewResolver tilesViewResolver() {
         return new TilesViewResolver();
+    }
+
+    @Bean
+    public HandlerInterceptor performanceInterceptor() {
+        PerformanceInterceptor interceptor;
+        interceptor = new PerformanceInterceptor();
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(performanceInterceptor()).addPathPatterns("/home", "/user/*");
     }
 }
 
